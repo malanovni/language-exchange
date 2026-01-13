047c6e51e9135702ae8491c58f30e2e582ea86fa
malanovni <tin.milinovic@acil.education>
2026-01-13 11:02:23 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    base: '/language-exchange/',
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/language-exchange/', // <-- This must be inside the return object
       server: {
         port: 3000,
         host: '0.0.0.0',
